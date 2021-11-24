622f7771c1c00d71df8fb5dfedddbc0e07d69900
Rahman Ahmed <rahmed83@uwo.ca>
2021-11-24 10:22:07 -0500

    Added placeholder connection string

--- DB/StockTrackerContext.cs
+++ DB/StockTrackerContext.cs
@@ -1,9 +1,20 @@
 ﻿using System;
+using IBM.Data.Db2;
 using Microsoft.EntityFrameworkCore;
 
 namespace StockTrackerService.Models
 {
 	public class StockTrackerContext : DbContext
 	{
+        private readonly DB2Connection _connection;
+    
+        public StockTrackerContext()
+        {
+            String connString = "";
+			_connection = new DB2Connection(connString);
+            _connection.Open();
+        }
+
+
 	}
 }
--- Startup.cs
+++ Startup.cs
@@ -8,10 +8,12 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using StockTrackerService.Data;
+using IBM.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using StockTrackerService.Models;
 
 namespace StockTrackerService
 {
@@ -28,6 +30,7 @@ namespace StockTrackerService
         public void ConfigureServices(IServiceCollection services)
         {
             //INITIALIZE DB HERE
+            services.AddDbContext<StockTrackerContext>(opt => opt.UseDb2());
             services.AddScoped<IStockListingRepo, StockListingRepo>();
             services.AddControllers();
             services.AddSwaggerGen(c =>
