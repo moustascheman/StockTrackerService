04052bc71c6f69ca521c1e11e5f5563e26fa82ae
Rahman Ahmed <rahmed83@uwo.ca>
2021-12-01 15:18:19 -0500

    Added code to enable service registration and deregistration on starr/stop.

--- Startup.cs
+++ Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -16,6 +17,7 @@ using System.Threading.Tasks;
 using StockTrackerService.Models;
 using Confluent.Kafka.DependencyInjection;
 using Confluent.Kafka;
+using System.Net.Http;
 
 namespace StockTrackerService
 {
@@ -32,9 +34,10 @@ namespace StockTrackerService
         public void ConfigureServices(IServiceCollection services)
         {
             //INITIALIZE DB HERE
-            services.AddDbContext<StockTrackerContext>(opt => opt.UseDb2(@Configuration.GetConnectionString("dbstring"), p=>p.SetServerInfo(IBMDBServerType.LUW)));
+            services.AddDbContext<StockTrackerContext>(opt => opt.UseDb2(@Configuration.GetConnectionString("dbstring"), p => p.SetServerInfo(IBMDBServerType.LUW)));
             services.AddScoped<IStockListingRepo, StockListingRepo>();
-                        
+            services.AddLogging();
+            services.AddHttpClient();
             services.AddSingleton<ProducerConfig>(opt => {
                 ProducerConfig config = new ProducerConfig();
                 config.BootstrapServers = Configuration["Kafka:default:BootstrapServers"];
@@ -53,7 +56,7 @@ namespace StockTrackerService
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IHostApplicationLifetime lifetime)
         {
             if (env.IsDevelopment())
             {
@@ -61,6 +64,10 @@ namespace StockTrackerService
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "StockTrackerService v1"));
             }
+
+            IHttpClientFactory clientFactory = app.ApplicationServices.GetService(typeof(IHttpClientFactory)) as IHttpClientFactory;
+            lifetime.ApplicationStarted.Register(RegisterToRegistry(clientFactory).Wait);
+            lifetime.ApplicationStopped.Register(() => DeregisterService(clientFactory));
 
             app.UseHttpsRedirection();
 
@@ -73,5 +80,42 @@ namespace StockTrackerService
                 endpoints.MapControllers();
             });
         }
+
+        private async Task<Action> RegisterToRegistry(IHttpClientFactory _builder)
+        {
+            HttpClient client = _builder.CreateClient();
+            string uri = Configuration["ServiceRegistry:uri"];
+            uri = uri + "/" + "StockTracker" + "/" + "localhost" + "/" + "49301" + "/" + "StockTracker" + "/" + 1;
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Put, uri);
+            try
+            {
+                HttpResponseMessage resp = await client.SendAsync(request);
+
+            }
+            catch
+            {
+                Console.WriteLine("Error when sending registration");
+            }
+
+            return null;
+        }
+        private Task<Action> DeregisterService(IHttpClientFactory factory)
+        {
+            HttpClient client = factory.CreateClient();
+            string uri = Configuration["ServiceRegistry:uri"];
+            uri = uri + "/" + "StockTracker";
+            HttpRequestMessage message = new HttpRequestMessage(HttpMethod.Delete, uri);
+            try
+            {
+                HttpResponseMessage resp = client.Send(message);
+                Console.WriteLine("Deregistered Service");
+            }
+            catch
+            {
+                Console.WriteLine("Error when deregistering service");
+            }
+            return null;
+        }
     }
+
 }
